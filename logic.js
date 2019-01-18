8a87197a7eafc41ffefb558ef30c9b6c3b0a8b63
Lars Hvam <larshp@hotmail.com>
2019-01-18 10:25:29 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,1 +1,16 @@
-console.log("hello");
+
+async function run() {
+  console.log("hello");
+  
+  const create = await octokit.checks.createSuite({
+    owner: "larshp", 
+    repo: "actions", 
+    head_sha: "sdfsd"});
+}
+
+run().then(text => {
+  process.exit();
+}).catch(err => {
+  console.dir(err);
+  process.exit();
+});
