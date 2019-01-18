f5b2394ec115ffb0783261f441f8fa448b212bd1
Lars Hvam <larshp@hotmail.com>
2019-01-18 11:15:27 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,10 +1,30 @@
 const octokit = require('@octokit/rest')();
 const fs = require('fs');
 
-async function run() {
-  
+function buildAnnotations() {
   const issues = JSON.parse(fs.readFileSync("/result.json", "utf-8"));
-  console.dir(issues);
+  const annotations = [];
+  
+  let count = 0;
+  for(let issue of issues) {
+    annotations.push({
+      path: issue.file,
+      start_line: issue.start.row,
+      end_line: issue.end.row,
+      title: issue.description,
+      annotation_level: "failure",
+      message: issue.key});
+    count = count + 1;
+    if (count === 50) {
+// only 50 annotations allowed, see https://developer.github.com/v3/checks/runs/
+      break;
+    }
+  }    
+  
+  return annotations;
+}  
+
+async function run() {
   
   octokit.authenticate({
     type: 'token',
@@ -12,6 +32,7 @@ async function run() {
   });
   
   const repo = process.env.GITHUB_REPOSITORY.split("/");
+  const annotations = buildAnnotations();
   
   const create = await octokit.checks.create({
     owner: repo[0], 
@@ -19,6 +40,7 @@ async function run() {
     name: "results",
     status: "completed",
     conclusion: "success",
+    output: {title: "Summary", summary: "Summary", annotations},
     completed_at: new Date().toISOString(),
     head_sha: process.env.GITHUB_SHA});
 }
