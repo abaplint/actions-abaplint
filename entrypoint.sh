35c94f441c6985ec41a6fcf9589f5616c5cdf343
Lars Hvam <larshp@hotmail.com>
2019-01-18 10:14:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@ set -x
 npm install abaplint -g
 pwd
 ls -l
-abaplint "src/**/*.*" -f json > ../result.json
+abaplint "src/**/*.*" -f json > /result.json
 ls ../
 ls /
 echo $GITHUB_SHA
 echo $GITHUB_REPOSITORY
 curl -H "Accept: application/vnd.github.antiope-preview+json" -H "Authorization: token $GITHUB_TOKEN" --request POST --data "{\"name\": \"hello world\", \"head_sha\": \"$GITHUB_SHA\"}" https://api.github.com/repos/$GITHUB_REPOSITORY/check-runs
+node logic.js
