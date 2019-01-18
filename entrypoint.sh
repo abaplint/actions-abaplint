1a5bcccb15646b67cb6231997e03804d39503fcd
Lars Hvam <larshp@hotmail.com>
2019-01-18 10:11:21 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ pwd
 ls -l
 abaplint "src/**/*.*" -f json > ../result.json
 ls ../
+ls /
 echo $GITHUB_SHA
 echo $GITHUB_REPOSITORY
 curl -H "Accept: application/vnd.github.antiope-preview+json" -H "Authorization: token $GITHUB_TOKEN" --request POST --data "{\"name\": \"hello world\", \"head_sha\": \"$GITHUB_SHA\"}" https://api.github.com/repos/$GITHUB_REPOSITORY/check-runs
