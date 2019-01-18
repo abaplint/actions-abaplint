5046b641f4d0243b012390aef652bdb9f2bf7ebb
Lars Hvam <larshp@hotmail.com>
2019-01-18 10:29:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -x
 npm install abaplint -g
-npm install @octokit/rest@16.10.0 -g
 pwd
 ls -l
 abaplint "src/**/*.*" -f json > /result.json
@@ -10,4 +9,7 @@ ls /
 echo $GITHUB_SHA
 echo $GITHUB_REPOSITORY
 curl -H "Accept: application/vnd.github.antiope-preview+json" -H "Authorization: token $GITHUB_TOKEN" --request POST --data "{\"name\": \"hello world\", \"head_sha\": \"$GITHUB_SHA\"}" https://api.github.com/repos/$GITHUB_REPOSITORY/check-runs
+
+cd /
+npm install @octokit/rest@16.10.0
 node /logic.js
