a54d24a7a3f3d10636ffe4c4092d2b115ae8ccb3
Lars Hvam <larshp@hotmail.com>
2019-01-18 10:58:01 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -10,9 +10,10 @@ async function run() {
   
   const repo = process.env.GITHUB_REPOSITORY.split("/");
   
-  const create = await octokit.checks.createSuite({
+  const create = await octokit.checks.create({
     owner: repo[0], 
     repo: repo[1], 
+    name: "foobar",
     head_sha: process.env.GITHUB_SHA});
 }
 
