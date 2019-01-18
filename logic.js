0ef4e5ddd3c4735e63744711e6b944daf32c2f60
Lars Hvam <larshp@hotmail.com>
2019-01-18 10:41:05 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,12 @@
 const octokit = require('@octokit/rest')();
 
 async function run() {
-  console.log("hello");
+  console.log("hello " + process.env.GITHUB_TOKEN);
+
+  octokit.authenticate({
+    type: 'token',
+    token: process.env.GITHUB_TOKEN,
+  });
   
   const create = await octokit.checks.createSuite({
     owner: "larshp", 
