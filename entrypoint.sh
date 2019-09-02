dcb5f155fc276fdffb76e709a8f0b3be7aeca695
Lars Hvam <larshp@hotmail.com>
2019-09-02 12:58:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 npm install abaplint -g
-abaplint -f total --outformat json --outfile /result.json
+abaplint -f total --outformat json --outfile result.json
+ls
 cd /
 ls
 npm install @octokit/rest@16.10.0 --loglevel=error
