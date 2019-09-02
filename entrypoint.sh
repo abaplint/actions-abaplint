6d5fb1df36eb5e73dbd7821a527e177a097fa92a
Lars Hvam <larshp@hotmail.com>
2019-09-02 13:01:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 npm install abaplint -g
-abaplint -f total --outformat json --outfile result.json
-ls
+abaplint -f total --outformat json --outfile /github/home/result.json
+ls /github/home/
 cd /
-ls
 npm install @octokit/rest@16.10.0 --loglevel=error
 node /logic.js
