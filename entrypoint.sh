048d1f82c61c860b36c7ca00fc2d2f270dbb3f97
larshp <larshp@hotmail.com>
2020-04-13 13:11:25 +0200

    install the specified version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $INPUT_VERSION
-npm install @abaplint/cli -g
+npm install @abaplint/cli@$INPUT_VERSION -g
 abaplint -f total --outformat json --outfile /result.json
 cd /
 npm install @octokit/rest@16.10.0 --loglevel=error
